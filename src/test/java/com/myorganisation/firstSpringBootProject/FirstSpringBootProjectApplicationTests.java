adc826a7fd9d64d8b93ea8ecaf481eb2cf3a4d35
anshit-1223 <81354203+anshit-1223@users.noreply.github.com>
2024-10-11 15:05:46 +0530

    Initial Commit

---
+++ src/test/java/com/myorganisation/firstSpringBootProject/FirstSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorganisation.firstSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
